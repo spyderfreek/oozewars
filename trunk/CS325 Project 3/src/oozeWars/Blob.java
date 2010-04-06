562ff43c5a9fafc284d6dbb53eeab07b508f5ce2
nkitten@gmail.com <nkitten@gmail.com@632c5525-702b-460d-3e6a-8d98454fbdac>
2010-04-06 23:28:13 +0000

    made some corrections to the way neighbors are gathered - some progress towards blobs which stick together.

--- trunk/CS325 Project 3/src/oozeWars/Blob.java
+++ trunk/CS325 Project 3/src/oozeWars/Blob.java
@@ -10,8 +10,8 @@ public class Blob extends Entity
 	private ArrayList<Particle> particles;
 	private Head head;
 	private Color color;
-	private double orientation, minSpeed = .5, maxSpeed = 8, friction = .9, accel, health = 0, blobForce = .6;
-	private double comfyDistance = 20;
+	private double orientation, minSpeed = .5, maxSpeed = 8, friction = .9, accel, health = 0, blobForce = 4;
+	private double comfyDistance = 10;
 	private int blobID;
 	
 	/**
