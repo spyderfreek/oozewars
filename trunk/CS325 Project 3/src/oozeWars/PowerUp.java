e3724e5feefe71bae20a94d8e35dc1f91399f476
sdf2897@email.vccs.edu <sdf2897@email.vccs.edu@632c5525-702b-460d-3e6a-8d98454fbdac>
2010-05-01 02:27:09 +0000

    PowerUp.java added for real this time.

---
+++ trunk/CS325 Project 3/src/oozeWars/PowerUp.java
@@ -0,0 +1,177 @@
+package oozeWars;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class PowerUp extends Particle 
+{
+	public static enum Type {GOD, NITRO, BOOST, GLUE, HEAL};
+	private Blob ownerBlob;
+	private boolean inBlob, collected;
+	private static int ticksOutsideBlob = 1350;
+	private int powerUpTicks;
+	private final Type type;
+
+	public PowerUp(double x, double y, double radius, Color color, Type type, int powerUpTicks, Game game) 
+	{
+		super(x, y, radius, color, 0);
+		inBlob = collected = false;
+		this.type = type;
+		
+		this.powerUpTicks = powerUpTicks;
+		Agent powerRemoverAgent = new Agent()
+							{ public void go(Game game, long timestep, int priorityLevel)
+								{
+									boolean isScheduled = false;
+									if(!isScheduled)
+									{
+										isScheduled = true;
+										game.queue.scheduleIn(ticksOutsideBlob, 1, this);
+									}
+									else if(!inBlob)
+									{
+										setDead(true);
+									}
+								}
+							};
+		game.queue.schedule(1, powerRemoverAgent);
+	}
+	
+	@Override
+	public void draw(Graphics2D graphics, Game game, Color col, double scale)
+	{
+		super.draw(graphics, game, color, scale);
+	}
+	
+	@Override
+	public void go(Game game, long timestep, int priorityLevel, 
+			double minSpeed, double maxSpeed, double frictn, double maxRadius, double growth )
+	{
+		if(blobID > 0 && !collected)
+		{
+			collected = true;
+			inBlob = true;
+			OozeWars ow = (OozeWars) game;
+			for(Blob b : ow.getBlobs())
+			{
+				if(b.getBlobID() == blobID)
+					ownerBlob = b;
+			}
+			
+			game.queue.schedule(0, createAgent(type));
+		}
+		super.go(game, timestep, priorityLevel, minSpeed, maxSpeed, frictn, maxRadius, growth);
+	}
+	
+	private Agent createAgent(Type type)
+	{
+		Agent pAgent;
+		
+		switch(type)
+		{
+			case GOD:
+			{
+				pAgent = new Agent()
+				{   boolean isSet = false;
+					public void go(Game game, long timestep, int priorityLevel)
+					{
+						if(!isSet)
+						{
+							isSet = true;
+							ownerBlob.setColor(color);
+							ownerBlob.setGod(true);
+							
+							game.queue.scheduleIn(powerUpTicks, 1, this);
+						}
+						else
+						{
+							ownerBlob.backToBaseColor();
+							ownerBlob.setGod(false);
+							setDead(true);
+						}
+					}
+				};
+				break;
+			}
+			case NITRO:
+			{
+				pAgent = new Agent()
+						{	boolean isSet = false;
+							public void go(Game game, long timestep, int priorityLevel)
+							{
+								if(!isSet)
+								{
+									isSet = true;
+									ownerBlob.setColor(color);
+									ownerBlob.setNitro(true);
+									
+									game.queue.scheduleIn(powerUpTicks, 1, this);
+								}
+								else
+								{
+									ownerBlob.backToBaseColor();
+									ownerBlob.setNitro(false);
+									setDead(true);
+								}
+							}
+						};
+				break;
+			}
+			case BOOST:
+			{
+				pAgent = new Agent()
+						{
+							public void go(Game game, long timestep, int priorityLevel)
+							{
+								ownerBlob.addParticles(10, (OozeWars) game);
+								ownerBlob.updateHealth();
+								setDead(true);
+							}
+						};
+				break;
+			}
+			case GLUE:
+			{
+				pAgent = new Agent()
+						{	boolean isSet = false;
+							double oldComf = ownerBlob.getComfyDistance();
+							
+							public void go(Game game, long timestep, int priorityLevel)
+							{
+								if(!isSet)
+								{
+									isSet = true;
+									ownerBlob.setColor(color);
+									ownerBlob.setComfyDistance(oldComf*.7);
+									
+									game.queue.scheduleIn(powerUpTicks, 1, this);
+								}
+								else
+								{
+									ownerBlob.backToBaseColor();
+									ownerBlob.setComfyDistance(oldComf);
+									setDead(true);
+								}
+							}
+						};
+				break;
+			}
+			case HEAL:
+			{
+				pAgent = new Agent()
+						{
+							public void go(Game game, long timestep, int priorityLevel)
+							{
+								ownerBlob.fullHeal();
+								setDead(true);
+							}
+						};
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Undefined Powerup Type");
+		}
+		
+		return pAgent;
+	}
+}
