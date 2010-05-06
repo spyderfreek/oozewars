14c079383489d295469985120c26e50050c48e84
sdf2897@email.vccs.edu <sdf2897@email.vccs.edu@632c5525-702b-460d-3e6a-8d98454fbdac>
2010-05-06 19:12:47 +0000

    Made it so OozeWars.java does not prompt the player for the number of players anymore.  Added a bunch of methods to MenuScreen.java, most of them do nothing atm.  Added switchToMain() and switchToPause() in MenuScreen.java so we can switch between the mainMenu and the pauseMenu

--- CS325 Project 3/src/oozeWars/MenuScreen.java
+++ CS325 Project 3/src/oozeWars/MenuScreen.java
@@ -12,12 +12,14 @@ public class MenuScreen extends JPanel implements ActionListener
 	private JPanel mainMenu, pauseMenu;
 	private JLabel title;
 	private OozeWars game;
-	private Box box;
-	JButton twoPlayer;
-	JButton instructions;
-	JButton highScores;
-	JButton about;
-	JButton quit;
+	private Box mainBox;
+	private Box pauseBox;
+	private JButton twoPlayer;
+	private JButton instructions;
+	private JButton highScores;
+	private JButton about;
+	private JButton quit;
+	private JButton resume;
 
 	public MenuScreen(OozeWars game, OozeView view) 
 	{
@@ -29,35 +31,25 @@ public class MenuScreen extends JPanel implements ActionListener
 		title = new JLabel("OozeWars", JLabel.CENTER);
 		this.add(title, BorderLayout.NORTH);
 		
-		box = new Box(BoxLayout.Y_AXIS);
+		mainBox = new Box(BoxLayout.Y_AXIS);
+		mainBox.add(new JLabel("Main Menu", JLabel.CENTER));
 		
-		twoPlayer = initializeButton("2 Players");
-		instructions = initializeButton("Instructions");
-		highScores = initializeButton("High Scores");
-		about = initializeButton("About");
-		quit = initializeButton("Quit");
+		twoPlayer = initializeButton("2 Players", mainBox);
+		instructions = initializeButton("Instructions", mainBox);
+		highScores = initializeButton("High Scores", mainBox);
+		about = initializeButton("About", mainBox);
+		quit = initializeButton("Quit", mainBox);
 		
-		box.add(Box.createGlue());
+		mainBox.add(Box.createGlue());
 		mainMenu = new JPanel();
-		mainMenu.add(box);
+		mainMenu.add(mainBox);
 		this.add(mainMenu, BorderLayout.CENTER);
-	}
-	
-	public void swapToGame()
-	{
-		Container container = getParent();
-		container.remove(this);
-		container.add(view, BorderLayout.CENTER);
-		view.requestFocus();
-	}
-	
-	private JButton initializeButton(String label)
-	{
-		JButton button = new JButton(label);
-		button.setActionCommand(label);
-		button.addActionListener(this);
-		box.add(button);
-		return button;
+		
+		pauseBox = new Box(BoxLayout.Y_AXIS);
+		pauseBox.add(new JLabel("Pause", JLabel.CENTER));
+		
+		resume = initializeButton("Resume", pauseBox);
+		pauseBox.add(quit);
 	}
 	
 	public void actionPerformed(ActionEvent e)
@@ -69,5 +61,79 @@ public class MenuScreen extends JPanel implements ActionListener
 			swapToGame();
 			game.reset();
 		}
+		else if(action.equals("Instructions"))
+			openInstructions();
+		else if(action.equals("High Scores"))
+			openHighScores();
+		else if(action.equals("About"))
+			openAbout();
+		else if(action.equals("Resume"))
+			resumeGame();
+		else if(action.equals("Quit"))
+		{
+			boolean answer = game.quit();
+			if(answer)
+				System.exit(0);
+		}
+	}
+	
+	private JButton initializeButton(String label, Box box)
+	{
+		JButton button = new JButton(label);
+		button.setActionCommand(label);
+		button.addActionListener(this);
+		box.add(button);
+		return button;
+	}
+	
+	public void swapToGame()
+	{
+		Container container = getParent();
+		container.remove(this);
+		container.add(view, BorderLayout.CENTER);
+		view.requestFocus();
+		validate();
+	}
+	
+	public void switchToPause()
+	{
+		try
+		{
+			this.remove(mainMenu);
+		}
+		catch (NullPointerException e){}
+		this.add(pauseMenu, BorderLayout.CENTER);
+		validate();
+	}
+	
+	public void switchToMain()
+	{
+		try
+		{
+			this.remove(pauseMenu);
+		}
+		catch(NullPointerException e){}
+		this.add(mainMenu, BorderLayout.CENTER);
+		validate();
+	}
+	
+	private void openInstructions()
+	{
+		
+	}
+	
+	private void openHighScores()
+	{
+		
+	}
+	
+	private void openAbout()
+	{
+		
+	}
+	
+	private void resumeGame()
+	{
+		
 	}
 }
